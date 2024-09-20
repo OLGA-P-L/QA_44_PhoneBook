11d516bb87338261e2c7cd069e80680f412191ea
OLGA-P-L <loyolya@gmail.com>
2024-09-20 08:45:06 +0300

    added HeaderMenuItem, added method clickButtonsOnHeader and realised in  negative registration tests

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import utils.HeaderMenuItem;
 
 public class BasePage {
     static WebDriver driver;
@@ -20,5 +22,24 @@ public class BasePage {
     public boolean isElementPresent(WebElement element, String text){
        return element.getText().contains(text);
 
+    }
+
+    public static <T extends BasePage> T clickButtonsOnHeader(HeaderMenuItem headerMenuitem){
+        WebElement element = driver.findElement(By.xpath(headerMenuitem.getLocator()));
+        element.click();
+        switch (headerMenuitem){
+            case HOME:
+                return (T) new HomePage(driver);
+
+            case ABOUT:
+                return (T) new AboutPage(driver);
+
+            case LOGIN:
+                return (T) new LoginPage(driver);
+
+            default:
+                throw new IllegalArgumentException("invalid parameter headerMenuItem");
+
+        }
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -7,30 +7,44 @@ import pages.HomePage;
 
 public class LoginTests extends ApplicationManager {
     @Test
-    public void loginPositiveTest(){
+    public void loginPositiveTest() {
         boolean result = new HomePage(getDriver()).clickBtnLoginHeader()
                 .typeLoginForm("qa_mail@mail.com", "Qwerty123!")
                 .clickBtnLoginPositive().isElementContactPresent();
         Assert.assertTrue(result);
-        }
+    }
 
-        @Test
-        public void loginNegativeTest_wrongPassword() {
-            Assert.assertTrue(new HomePage(getDriver()).clickBtnLoginHeader()
-                    .typeLoginForm("qa_mail@mail.com", "Qwerty123!!!")
-                    .clickBtnLoginNegative().closeAlert().isTextInElementPresent_errorMessage());
-        }
+    @Test
+    public void loginNegativeTest_wrongPassword() {
+        Assert.assertTrue(new HomePage(getDriver()).clickBtnLoginHeader()
+                .typeLoginForm("qa_mail@mail.com", "Qwerty123!!!")
+                .clickBtnLoginNegative().closeAlert().isTextInElementPresent_errorMessage());
+    }
 
-            @Test
-            public void loginNegativeTest_wrongEmail(){
-                new HomePage(getDriver()).clickBtnLoginHeader()
-                        .typeLoginForm("qa_mail@mailcom", "Qwerty123!!!")
-                        .clickBtnLoginNegative().closeAlert().isTextInElementPresent_errorMessage();
+    //@Test
+    //public void loginNegativeTest_wrongEmail() {
+    //new HomePage(getDriver()).clickBtnLoginHeader()
+    //            .typeLoginForm("qa_mail1@mail.com", "Qwerty123!")
+    //            .clickBtnLoginNegative().closeAlert().isTextInElementPresent_errorMessage();
+    //}
 
+    @Test
+    public void loginNegativeTest_wrongEmailUnregUser() {
+        Assert.assertTrue(new HomePage(getDriver()).clickBtnLoginHeader()
+                .typeLoginForm("qa_mail1@mail.com", "Qwerty123!")
+                .clickBtnLoginNegative().closeAlert().isTextInElementPresent_errorMessage());
+    }
+
+    @Test
+    public void loginNegativeTest_wrongEmailWOAt() {
+        Assert.assertTrue(new HomePage(getDriver()).clickBtnLoginHeader()
+                .typeLoginForm("qa_mail@mailcom", "Qwerty123!")
+                .clickBtnLoginNegative().closeAlert().isTextInElementPresent_errorMessage());
 
         //Assert.assertEquals(100,200);
         //Assert.assertTrue(true);
         //Assert.assertFalse(false);
         //Assert.assertNotEquals(100,100);
+
     }
 }
